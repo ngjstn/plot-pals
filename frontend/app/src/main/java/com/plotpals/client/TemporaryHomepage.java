97397cbb49fe5d8253d7b9b73d21af26df4586f1
James T Ardian <jamesardian01@gmail.com>
2023-10-05 09:47:30 -0700

    front-end template

---
+++ frontend/app/src/main/java/com/plotpals/client/TemporaryHomepage.java
@@ -0,0 +1,21 @@
+package com.plotpals.client;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * Temporary home page to route to feature pages
+ */
+public class TemporaryHomepage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /**
+         * Insert buttons for feature pages here
+         */
+    }
+}

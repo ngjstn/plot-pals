2dc735e36c54f9ed0a77907786d377b2b28823a5
James T Ardian <jamesardian01@gmail.com>
2023-10-09 22:59:15 -0700

    run skip profile creation for CreateProfileActivity in onstart

--- frontend/app/src/main/java/com/plotpals/client/CreateProfileActivity.java
+++ frontend/app/src/main/java/com/plotpals/client/CreateProfileActivity.java
@@ -15,6 +15,8 @@ import com.android.volley.RequestQueue;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.plotpals.client.utils.GoogleProfileInformation;
 import com.squareup.picasso.Picasso;
 
@@ -43,7 +45,6 @@ public class CreateProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         loadExtras();
-        skipProfileCreationIfUserProfileAlreadyExists();
 
         setContentView(R.layout.activity_create_profile);
 
@@ -66,8 +67,13 @@ public class CreateProfileActivity extends AppCompatActivity {
         CheckmarkImageView.setOnClickListener((view) -> {
             createProfileAndRedirect();
         });
+    }
 
-
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        skipProfileCreationIfUserProfileAlreadyExists();
     }
 
     private void createProfileAndRedirect() {

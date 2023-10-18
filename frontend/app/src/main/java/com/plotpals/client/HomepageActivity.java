4c7af2d5f4bb854bbd105aa69485fe9226e4920c
James T Ardian <jamesardian01@gmail.com>
2023-10-17 23:38:20 -0700

    add a way to access actual homepage through temporary homepage

--- frontend/app/src/main/java/com/plotpals/client/HomepageActivity.java
+++ frontend/app/src/main/java/com/plotpals/client/HomepageActivity.java
@@ -3,12 +3,28 @@ package com.plotpals.client;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.plotpals.client.utils.GoogleProfileInformation;
 
 public class HomepageActivity extends AppCompatActivity {
 
+    TextView HomepageTitleTextView;
+    GoogleProfileInformation googleProfileInformation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+        loadExtras();
+
+        HomepageTitleTextView = findViewById(R.id.homepage_title_text_view);
+    }
+
+    private void loadExtras() {
+        Bundle extras = getIntent().getExtras();
+
+        if (extras != null) {
+            googleProfileInformation = new GoogleProfileInformation(extras);
+        }
     }
 }
--- frontend/app/src/main/java/com/plotpals/client/TemporaryHomepageActivity.java
+++ frontend/app/src/main/java/com/plotpals/client/TemporaryHomepageActivity.java
@@ -8,21 +8,34 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.plotpals.client.utils.GoogleProfileInformation;
+
 /**
  * Temporary home page to route to feature pages
  */
 public class TemporaryHomepageActivity extends AppCompatActivity {
     final static String TAG = "TemporaryHomepageActivity";
+
+    GoogleProfileInformation googleProfileInformation;
+    private Button actualHomepageButton;
     private Button mapsButton;
     private Button accountButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temporary_homepage);
-
+        loadExtras();
         /**
          * Insert buttons for feature pages here
          */
+
+        actualHomepageButton = findViewById(R.id.actual_homepage_button);
+        actualHomepageButton.setOnClickListener(view -> {
+            Log.d(TAG, "Clicking Homepage Button");
+            Intent homepageIntent = new Intent(TemporaryHomepageActivity.this, HomepageActivity.class);
+            googleProfileInformation.loadGoogleProfileInformationToIntent(homepageIntent);
+            startActivity(homepageIntent);
+        });
 
         mapsButton = findViewById(R.id.maps_button);
         mapsButton.setOnClickListener(new View.OnClickListener() {
@@ -40,5 +53,13 @@ public class TemporaryHomepageActivity extends AppCompatActivity {
             Intent accountIntent = new Intent(TemporaryHomepageActivity.this, AccountMainActivity.class);
             startActivity(accountIntent);
         });
+    }
+
+    private void loadExtras() {
+        Bundle extras = getIntent().getExtras();
+
+        if (extras != null) {
+            googleProfileInformation = new GoogleProfileInformation(extras);
+        }
     }
 }

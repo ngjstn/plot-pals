0fd04f6d4fc7c2aaf7026bd190fc17c8494c6e51
James T Ardian <jamesardian01@gmail.com>
2023-10-26 16:07:09 -0700

    format and add garden name

--- mysql_cheatsheet.sql
+++ mysql_cheatsheet.sql
@@ -21,13 +21,88 @@ SELECT * FROM reports;
 SELECT * FROM updates;
 
 /* INSERT STATEMENTS TEMPLATE TO INITIALIZE TEST DATA FOR NON-PROFILE TABLES */
-INSERT INTO gardens(address, longitude, latitude, gardenOwnerId, isApproved, gardenPicture, contactPhoneNumber, contactEmail, numberOfPlots) 
-	VALUES ('some address', 38.8951 /*some longitude with 4 decimals*/, -77.0364 /*some latitude with 4 decimals*/, '{insert some profile.id}', false, NULL /*deal with pictures later*/, 'some phone number', 'some email', 10 /*any int you want*/); 
-INSERT INTO plots(gardenId, plotOwnerId) VALUES (123 /*some int from garden.id */, '{insert some profile.id}');
-INSERT INTO tasks(plotId, reward, minimumRating, description, assignerId, assigneeId, isCompleted, assigneeIsProvidedFeedback, gardenId, deadlineDate, taskStartTime, taskEndTime, expectedTaskDurationInHours)
-	VALUES (123 /*some plot.id int*/, 'some reward', 4.56 /*some number between 0-5 with 2 decimals*/, 'some desc', '{insert some profile.id}', '{insert some profile.id}', false, false, 123 /*some garden id*/, '2023-04-01 10:00:00' /*YYYY-MM-DD*/, '2023-01-01 10:00:00' /*YYYY-MM-DD*/, '2023-03-01 10:00:00' /*YYYY-MM-DD*/, 100 /*some int*/);
-INSERT INTO reports(reportedId, reporteeId, reason, comment) VALUES ('{insert some profile.id}', '{insert some profile.id}', "some reason", "some comment");
-INSERT INTO updates(userId, description, title) VALUES ('{insert your profile.id}', "some test description", "some title");
+INSERT INTO gardens(
+	address, 
+    longitude, 
+    latitude, 
+    gardenOwnerId, 
+    isApproved, 
+    gardenPicture, 
+    contactPhoneNumber, 
+    contactEmail, 
+    numberOfPlots, 
+    gardenName
+) VALUES (
+    'some address', 
+    38.8951 /*some longitude with 4 decimals*/, 
+    -77.0364 /*some latitude with 4 decimals*/, 
+    '{insert some profile.id}', 
+    false, 
+    NULL /*deal with pictures later*/, 
+    'some phone number', 
+    'some email',
+    10 /*any int you want*/, 
+    'some garden name'
+); 
+
+INSERT INTO plots(
+	gardenId, 
+	plotOwnerId
+) VALUES (
+	123 /*some int from garden.id */, 
+	'{insert some profile.id}'
+);
+
+INSERT INTO tasks(
+	plotId, 
+	reward, 
+	minimumRating, 
+	description, 
+	assignerId, 
+	assigneeId, 
+	isCompleted, 
+	assigneeIsProvidedFeedback, 
+	gardenId, 
+	deadlineDate, 
+	taskStartTime, 
+	taskEndTime, 
+	expectedTaskDurationInHours
+) VALUES (
+	123 /*some plot.id int*/, 
+	'some reward', 
+	4.56 /*some number between 0-5 with 2 decimals*/, 
+	'some desc', 
+	'{insert some profile.id}', 
+	'{insert some profile.id}', 
+	false, 
+	false, 
+	123 /*some garden id*/, 
+	'2023-04-01 10:00:00' /*YYYY-MM-DD*/, 
+	'2023-01-01 10:00:00' /*YYYY-MM-DD*/, 
+	'2023-03-01 10:00:00' /*YYYY-MM-DD*/, 
+	100 /*some int*/
+);
+INSERT INTO reports(
+	reportedId, 
+	reporteeId, 
+	reason, 
+	comment
+) VALUES (
+	'{insert some profile.id}',
+	'{insert some profile.id}', 
+	'some reason', 
+	'some comment'
+);
+
+INSERT INTO updates(
+	userId, 
+	description, 
+	title
+) VALUES (
+	'{insert your profile.id}', 
+	"some test description", 
+	"some title"
+);
 
 /* UPDATE STATEMENT TEMPLATE */
 /* UPDATE table_name SET column1 = value1, column2 = value2, ... WHERE condition; */

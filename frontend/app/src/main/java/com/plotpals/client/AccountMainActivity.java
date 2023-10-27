8ed422e3cc2e867f8d31a9632036db0b8c911659
adrienneyl <adrienne.yl07@gmail.com>
2023-10-26 20:18:00 -0700

    Draft for new garden application first page

--- frontend/app/src/main/java/com/plotpals/client/AccountMainActivity.java
+++ frontend/app/src/main/java/com/plotpals/client/AccountMainActivity.java
@@ -2,13 +2,29 @@ package com.plotpals.client;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class AccountMainActivity extends AppCompatActivity {
-
+    final static String TAG = "AccountMainActivity";
+    private View applyButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_main);
+
+        applyButton = findViewById(R.id.account_apply_button);
+        applyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "Clicking Garden App button");
+                Intent gardenAppIntent = new Intent(AccountMainActivity.this, GardenApplicationActivity.class);
+                startActivity(gardenAppIntent);
+            }
+        });
+
     }
 }

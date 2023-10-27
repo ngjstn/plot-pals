1921b437475eb112e589355bcfb50ade5cc198e9
alan <alanshen111@gmail.com>
2023-10-27 15:26:50 -0700

    added tag

--- frontend/app/src/main/java/com/plotpals/client/MyGardenNoGardenActivity.java
+++ frontend/app/src/main/java/com/plotpals/client/MyGardenNoGardenActivity.java
@@ -6,7 +6,7 @@ import android.util.Log;
 import android.widget.Button;
 
 public class MyGardenNoGardenActivity extends NavBarActivity {
-
+    final static String TAG = "MyGardenNoGardenActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

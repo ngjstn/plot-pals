2935f4e4cff51ae598904c7e7edb63f39f9fcedf
adrienneyl <adrienne.yl07@gmail.com>
2023-10-26 20:18:00 -0700

    Merging in main changes to use fragments

--- frontend/app/src/main/java/com/plotpals/client/AccountMainActivity.java
+++ frontend/app/src/main/java/com/plotpals/client/AccountMainActivity.java
@@ -4,12 +4,30 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class AccountMainActivity extends NavBarActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
+public class AccountMainActivity extends NavBarActivity {
+    final static String TAG = "AccountMainActivity";
+    private View applyButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_main);
+
+        applyButton = findViewById(R.id.account_apply_button);
+        applyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "Clicking Garden App button");
+                Intent gardenAppIntent = new Intent(AccountMainActivity.this, GardenApplicationActivity.class);
+                startActivity(gardenAppIntent);
+            }
+        });
+
 
         ActivateNavBar();
     }

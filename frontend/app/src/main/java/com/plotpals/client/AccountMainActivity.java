5b5e5aa963a0adbd935e6a584acbcd16d6fc698f
adrienneyl <adrienne.yl07@gmail.com>
2023-10-27 19:02:52 -0700

    Fragments seem to work, need to test with inputs and transitioning back and forth

--- frontend/app/src/main/java/com/plotpals/client/AccountMainActivity.java
+++ frontend/app/src/main/java/com/plotpals/client/AccountMainActivity.java
@@ -28,7 +28,6 @@ public class AccountMainActivity extends NavBarActivity {
             }
         });
 
-
         ActivateNavBar();
     }
 }
---
+++ frontend/app/src/main/java/com/plotpals/client/FragGAInfoPage.java
@@ -0,0 +1,27 @@
+package com.plotpals.client;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragGAInfoPage extends Fragment {
+    public FragGAInfoPage() {
+        // Empty constructor is fine
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_ga_garden_info, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // Any additional code for when the view has been created
+    }
+}

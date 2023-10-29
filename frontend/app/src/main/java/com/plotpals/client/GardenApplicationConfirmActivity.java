2526baec3b6182888ad426677d3d9c9445df3fc2
adrienneyl <adrienne.yl07@gmail.com>
2023-10-29 16:16:35 -0700

    Draft for form - need to add error-checking, back-end endpoints, bug with extras

--- frontend/app/src/main/java/com/plotpals/client/GardenApplicationConfirmActivity.java
+++ frontend/app/src/main/java/com/plotpals/client/GardenApplicationConfirmActivity.java
@@ -5,20 +5,86 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+import com.plotpals.client.utils.GoogleProfileInformation;
+
+import java.util.HashMap;
 
 public class GardenApplicationConfirmActivity extends AppCompatActivity {
+    GoogleProfileInformation googleProfileInformation;
     private Button confirmButton;
+    private String gardenName;
+    private String gardenAddress;
+    private String gardenPlots;
+    private String gardenContactName;
+    private String gardenPhone;
+    private String gardenEmail;
+    private TextView gardenNameDisplay;
+    private TextView gardenAddressDisplay;
+    private TextView gardenPlotsDisplay;
+    private TextView gardenContactNameDisplay;
+    private TextView gardenPhoneDisplay;
+    private TextView gardenEmailDisplay;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        loadExtras();
         setContentView(R.layout.activity_garden_application_confirm);
+
+        Bundle applicationExtras = getIntent().getExtras();
+        gardenName = applicationExtras.getString("gardenName");
+        gardenAddress = applicationExtras.getString("gardenAddress");
+        gardenPlots = applicationExtras.getString("gardenPlots");
+        gardenPhone = applicationExtras.getString("gardenPhone");
+        gardenEmail = applicationExtras.getString("gardenEmail");
+
+        gardenNameDisplay = findViewById(R.id.garden_name_placeholder);
+        gardenAddressDisplay = findViewById(R.id.garden_address_placeholder);
+        gardenPlotsDisplay = findViewById(R.id.garden_plot_placeholder);
+        gardenContactNameDisplay = findViewById(R.id.garden_contact_name_placeholder);
+        gardenPhoneDisplay = findViewById(R.id.garden_contact_phone_placeholder);
+        gardenEmailDisplay = findViewById(R.id.garden_contact_email_placeholder);
+
+        gardenNameDisplay.setText(gardenName);
+        gardenAddressDisplay.setText(gardenAddress);
+        gardenPlotsDisplay.setText(gardenPlots);
+        gardenPhoneDisplay.setText(gardenPhone);
+        gardenEmailDisplay.setText(gardenEmail);
+        gardenContactNameDisplay.setText(googleProfileInformation.getAccountGoogleName());
 
         confirmButton = findViewById(R.id.confirm_application_button);
         confirmButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // sendGardenInformation();
                 finish();
             }
         });
+    }
+
+    private void sendGardenInformation() {
+        // checks should have already happened
+
+        RequestQueue volleyQueue = Volley.newRequestQueue(this);
+        HashMap<String, String> params = new HashMap<>();
+        params.put("gardenName", gardenName);
+        params.put("gardenAddress", gardenAddress);
+        params.put("gardenPlots", gardenPlots);
+        params.put("gardenPhone", gardenPhone);
+        params.put("gardenEmail", gardenEmail);
+
+        String url = "http://10.0.2.2:8081/garden_applications";
+
+    }
+
+    private void loadExtras() {
+        Bundle extras = getIntent().getExtras();
+
+        if (extras != null) {
+            googleProfileInformation = new GoogleProfileInformation(extras);
+        }
     }
 }

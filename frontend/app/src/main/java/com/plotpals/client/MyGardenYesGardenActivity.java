7496ccb2f74c4d4307978a5edd0dda2b9eba2b45
alan <alanshen111@gmail.com>
2023-10-29 09:25:41 -0700

    adding buttons, icons

--- frontend/app/src/main/java/com/plotpals/client/MyGardenYesGardenActivity.java
+++ frontend/app/src/main/java/com/plotpals/client/MyGardenYesGardenActivity.java
@@ -1,14 +1,41 @@
 package com.plotpals.client;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.plotpals.client.utils.GoogleProfileInformation;
 
 public class MyGardenYesGardenActivity extends NavBarActivity {
     final static String TAG = "MyGardenYesGardenActivity";
+    GoogleProfileInformation googleProfileInformation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_garden_yes_garden);
-
+        loadExtras();
         activateNavBar();
+
+        ImageView plusButton = findViewById(R.id.my_garden_plus_button);
+        plusButton.setOnClickListener(view -> {
+            Log.d(TAG, "Clicking Google Maps Button");
+            Intent mapsIntent = new Intent(MyGardenYesGardenActivity.this, MapsActivity.class);
+            googleProfileInformation.loadGoogleProfileInformationToIntent(mapsIntent);
+            startActivity(mapsIntent);
+        });
+        
+    }
+
+    /**
+     * load extras forwarded from previous activity
+     */
+    private void loadExtras() {
+        Bundle extras = getIntent().getExtras();
+
+        if (extras != null) {
+            googleProfileInformation = new GoogleProfileInformation(extras);
+        }
     }
 }

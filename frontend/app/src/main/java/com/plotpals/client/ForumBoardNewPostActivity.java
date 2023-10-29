96fb675b9d54f5161b54d9a4c3b1d8c73dbc8681
alan <alanshen111@gmail.com>
2023-10-29 15:50:13 -0700

    yea

---
+++ frontend/app/src/main/java/com/plotpals/client/ForumBoardNewPostActivity.java
@@ -0,0 +1,61 @@
+package com.plotpals.client;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.plotpals.client.utils.GoogleProfileInformation;
+
+public class ForumBoardNewPostActivity extends NavBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forum_board_new_post);
+        loadExtras();
+        activateNavBar();
+
+        ImageView x = findViewById(R.id.forum_board_new_post_x);
+        x.setOnClickListener(view -> {
+            Log.d(TAG, "Clicking X");
+            Intent intent = new Intent(ForumBoardNewPostActivity.this, ForumBoardMainActivity.class);
+            googleProfileInformation.loadGoogleProfileInformationToIntent(intent);
+            startActivity(intent);
+        });
+
+        ImageView check = findViewById(R.id.forum_board_new_post_check);
+        check.setOnClickListener(view -> {
+            Log.d(TAG, "Clicking Check");
+
+            EditText titleText = (EditText)findViewById(R.id.forum_board_new_post_title);
+            EditText bodyText = (EditText)findViewById(R.id.forum_board_new_post_body);
+
+            if (titleText.getText().toString().trim().equals("") ||
+                    bodyText.getText().toString().trim().equals("")) {
+                Toast.makeText(ForumBoardNewPostActivity.this, "Please enter a Title/Body", Toast.LENGTH_SHORT).show();
+            } else {
+                Intent intent = new Intent(ForumBoardNewPostActivity.this, ForumBoardMainActivity.class);
+                googleProfileInformation.loadGoogleProfileInformationToIntent(intent);
+                Toast.makeText(ForumBoardNewPostActivity.this, "Post posted (no backend)", Toast.LENGTH_SHORT).show();
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+    /**
+     * load extras forwarded from previous activity
+     */
+    private void loadExtras() {
+        Bundle extras = getIntent().getExtras();
+
+        if (extras != null) {
+            googleProfileInformation = new GoogleProfileInformation(extras);
+        }
+    }
+
+}

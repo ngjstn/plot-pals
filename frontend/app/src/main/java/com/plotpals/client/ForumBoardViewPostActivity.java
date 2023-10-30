9836d74f9f17df4c97e64476c0dadf06b4e800e2
alan <alanshen111@gmail.com>
2023-10-30 11:55:19 -0700

    starting

---
+++ frontend/app/src/main/java/com/plotpals/client/ForumBoardViewPostActivity.java
@@ -0,0 +1,14 @@
+package com.plotpals.client;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ForumBoardViewPostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forum_board_view_post);
+    }
+}

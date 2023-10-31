07579f4ecb1c15f29904e33c4f37930202aed11e
alan <alanshen111@gmail.com>
2023-10-31 15:28:10 -0700

    preview view

--- frontend/app/src/main/java/com/plotpals/client/ForumBoardMainActivity.java
+++ frontend/app/src/main/java/com/plotpals/client/ForumBoardMainActivity.java
@@ -57,13 +57,13 @@ public class ForumBoardMainActivity extends NavBarActivity {
             startActivity(intent);
         });
 
-        TextView title = findViewById(R.id.forum_board_title1);
-        title.setOnClickListener(view -> {
-            Log.d(TAG, "Clicking Task Title");
-            Intent intent = new Intent(ForumBoardMainActivity.this, ForumBoardViewTaskActivity.class);
-            googleProfileInformation.loadGoogleProfileInformationToIntent(intent);
-            startActivity(intent);
-        });
+//        TextView title = findViewById(R.id.forum_board_title1);
+//        title.setOnClickListener(view -> {
+//            Log.d(TAG, "Clicking Task Title");
+//            Intent intent = new Intent(ForumBoardMainActivity.this, ForumBoardViewTaskActivity.class);
+//            googleProfileInformation.loadGoogleProfileInformationToIntent(intent);
+//            startActivity(intent);
+//        });
 
     }
 

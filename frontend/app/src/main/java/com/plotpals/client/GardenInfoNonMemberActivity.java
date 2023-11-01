68c7c178d505ff0f2990f17361524ff253ce524f
Justin Ng <89366060+ngjstn@users.noreply.github.com>
2023-11-01 12:47:46 -0700

    non-member joining garden as caretaker + UI changes (#40)

--- frontend/app/src/main/java/com/plotpals/client/GardenInfoNonMemberActivity.java
+++ frontend/app/src/main/java/com/plotpals/client/GardenInfoNonMemberActivity.java
@@ -9,12 +9,18 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.plotpals.client.data.Garden;
+import com.plotpals.client.data.Role;
+import com.plotpals.client.data.RoleEnum;
 import com.plotpals.client.utils.GoogleProfileInformation;
 
 import org.json.JSONArray;
@@ -22,6 +28,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.w3c.dom.Text;
 
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -48,17 +55,14 @@ public class GardenInfoNonMemberActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Toast.makeText(GardenInfoNonMemberActivity.this, "Join pressed", Toast.LENGTH_SHORT).show();
+                try {
+                    joinGardenRole(RoleEnum.CARETAKER);
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
             }
         });
 
-//        findViewById(R.id.arrow_back_).setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Toast.makeText(GardenInfoNonMemberActivity.this, "Back Arrow pressed", Toast.LENGTH_SHORT).show();
-//                Intent mapsActivity = new Intent(GardenInfoNonMemberActivity.this, MapsActivity.class);
-//                startActivity(mapsActivity);
-//            }
-//        });
         findViewById(R.id.arrow_back_).setOnClickListener(view -> finish());
     }
 
@@ -102,6 +106,48 @@ public class GardenInfoNonMemberActivity extends AppCompatActivity {
         };
 
         volleyQueue.add(jsonObjectRequest);
+    }
+
+    private void joinGardenRole(RoleEnum role) throws JSONException {
+        RequestQueue volleyQueue = Volley.newRequestQueue(this);
+        String url = "http://10.0.2.2:8081/roles";
+        JSONObject jsonBody = new JSONObject();
+        jsonBody.put("roleNum", role.ordinal());
+        jsonBody.put("gardenId", gardenId);
+        jsonBody.put("profileId", googleProfileInformation.getAccountUserId());
+        final String requestBody = jsonBody.toString();
+        StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d(TAG, String.format("POST joining garden as %s", role.toString()));
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }) {
+            @Override
+            public String getBodyContentType() {
+                return "application/json; charset=utf-8";
+            }
+            @Override
+            public byte[] getBody() throws AuthFailureError {
+                try {
+                    return requestBody == null ? null : requestBody.getBytes("utf-8");
+                } catch (UnsupportedEncodingException uee) {
+                    VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", requestBody, "utf-8");
+                    return null;
+                }
+            }
+            @Override
+            public Map<String, String> getHeaders() {
+                HashMap<String, String> headers = new HashMap<>();
+                headers.put("Authorization", "Bearer " + googleProfileInformation.getAccountIdToken());
+                return headers;
+            }
+        };
+        volleyQueue.add(request);
     }
 
     private void updateGardenOverlayContent(Garden garden) {

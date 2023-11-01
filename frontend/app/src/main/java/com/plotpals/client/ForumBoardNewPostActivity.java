d4920fc6bf4342e2edb7a74dd8e52853304cc36a
adrienne leung <73402347+adrienneyl@users.noreply.github.com>
2023-11-01 15:09:27 -0700

    New post and task backend functionality (fixed) (#45)

--- frontend/app/src/main/java/com/plotpals/client/ForumBoardNewPostActivity.java
+++ frontend/app/src/main/java/com/plotpals/client/ForumBoardNewPostActivity.java
@@ -7,10 +7,25 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.plotpals.client.utils.GoogleProfileInformation;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ForumBoardNewPostActivity extends NavBarActivity {
     final static String TAG = "ForumBoardNewPostActivity";
+    private EditText titleText;
+    private EditText bodyText;
+    private Integer currentGardenId;
+    private String currentGardenName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,8 +45,8 @@ public class ForumBoardNewPostActivity extends NavBarActivity {
         check.setOnClickListener(view -> {
             Log.d(TAG, "Clicking Check");
 
-            EditText titleText = (EditText)findViewById(R.id.forum_board_new_post_title);
-            EditText bodyText = (EditText)findViewById(R.id.forum_board_new_post_body);
+            titleText = (EditText)findViewById(R.id.forum_board_new_post_title);
+            bodyText = (EditText)findViewById(R.id.forum_board_new_post_body);
 
             if (titleText.getText().toString().trim().equals("") ||
                     bodyText.getText().toString().trim().equals("")) {
@@ -39,12 +54,51 @@ public class ForumBoardNewPostActivity extends NavBarActivity {
             } else {
                 Intent intent = new Intent(ForumBoardNewPostActivity.this, ForumBoardMainActivity.class);
                 googleProfileInformation.loadGoogleProfileInformationToIntent(intent);
+                intent.putExtra("gardenId", currentGardenId);
+                intent.putExtra("gardenName", currentGardenName);
                 Toast.makeText(ForumBoardNewPostActivity.this, "Post posted (no backend)", Toast.LENGTH_SHORT).show();
+
+                sendPostInformation();
+
                 startActivity(intent);
             }
         });
+    }
 
+    private void sendPostInformation() {
 
+        RequestQueue volleyQueue = Volley.newRequestQueue(this);
+        HashMap<String, String> params = new HashMap<>();
+        params.put("postTitle", titleText.getText().toString());
+        params.put("postDesc", bodyText.getText().toString());
+
+        String url = String.format("http://10.0.2.2:8081/posts?gardenId=%s", currentGardenId);
+
+        Request<?> jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.POST,
+                url,
+                new JSONObject(params),
+                (JSONObject response) -> {
+                    try {
+                        Log.d(TAG, "Response for submitting form: \n"
+                                + response.getString("success"));
+                    } catch (JSONException e) {
+                        Log.d(TAG, e.toString());
+                    }
+                },
+                (VolleyError e) -> {
+                    Log.d(TAG, e.toString());
+                }
+        ) {
+            @Override
+            public Map<String, String> getHeaders() {
+                HashMap<String, String> headers = new HashMap<>();
+                headers.put("Authorization", "Bearer " + googleProfileInformation.getAccountIdToken());
+                return headers;
+            }
+        };
+
+        volleyQueue.add(jsonObjectRequest);
     }
 
     /**
@@ -55,6 +109,8 @@ public class ForumBoardNewPostActivity extends NavBarActivity {
 
         if (extras != null) {
             googleProfileInformation = new GoogleProfileInformation(extras);
+            currentGardenId = extras.getInt("gardenId");
+            currentGardenName = extras.getString("gardenName");
         }
     }
 

fa5b94f9e41d0b083f4e8dc396549b9a78304354
alan <alanshen111@gmail.com>
2023-11-01 21:17:34 -0700

    starting

---
+++ frontend/app/src/main/java/com/plotpals/client/ForumBoardFeedbackActivity.java
@@ -0,0 +1,14 @@
+package com.plotpals.client;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ForumBoardFeedbackActivity extends NavBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forum_board_feedback);
+    }
+}
--- frontend/app/src/main/java/com/plotpals/client/ForumBoardViewTaskActivity.java
+++ frontend/app/src/main/java/com/plotpals/client/ForumBoardViewTaskActivity.java
@@ -24,6 +24,7 @@ public class ForumBoardViewTaskActivity extends NavBarActivity {
     private String taskReward;
     private String taskAssignee;
     private String taskAssigneeId;
+    private String taskAssignerId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,7 +61,14 @@ public class ForumBoardViewTaskActivity extends NavBarActivity {
         Log.d(TAG, "Google Id: " + googleProfileInformation.getAccountUserId());
 
         Button button = findViewById(R.id.forum_board_task_button);
-        if (taskStatus) { // task is complete
+        if (taskStatus && taskAssignerId.equals(googleProfileInformation.getAccountUserId())) { // task is complete and we made it
+            button.setText("Provide Feedback");
+            button.setOnClickListener(view -> {
+                Toast.makeText(ForumBoardViewTaskActivity.this, "Provide Feedback Button Pressed", Toast.LENGTH_SHORT).show();
+                // set assignee and start time?
+                button.setVisibility(View.GONE);
+            });
+        } else if (taskStatus) { // task is complete
             button.setVisibility(View.GONE);
         } else if (taskAssignee == null || taskAssignee.equals("null")) { // nobody is assigned
             button.setText("Volunteer for this task");
@@ -103,6 +111,7 @@ public class ForumBoardViewTaskActivity extends NavBarActivity {
             taskReward = extras.getString("taskReward");
             taskAssignee = extras.getString("taskAssignee");
             taskAssigneeId = extras.getString("taskAssigneeId");
+            taskAssignerId = extras.getString("taskAssignerId");
         }
     }
 }

6f48c29b3cc6fd10ee173a35f5e506cae8360a25
James T Ardian <jamesardian01@gmail.com>
2023-11-02 11:56:40 -0700

    competence calculation

--- backend/controllers/profilesController.js
+++ backend/controllers/profilesController.js
@@ -27,7 +27,7 @@ const createProfileForAuthenticatedUser = async (req, res, next) => {
 };
 
 const submitFeedback = async (req, res, next) => {
-  const { newRating } = req.body;
+  const { newRating, taskId } = req.body;
   let oldRating;
   let calculatedRating;
 
@@ -40,12 +40,34 @@ const submitFeedback = async (req, res, next) => {
     console.log(err);
     return next(err);
   }
-  console.log("old rating: " + oldRating);
-  // calculate rating
-  calculatedRating = oldRating * 0.8 + newRating * 0.2;
-  console.log("calculated rating: " + calculatedRating);
+
+  // calculate boost in ratings due to completion efficiency
+  let startTime;
+  let endTime;
+  let expectedTaskDurationInHours;
+  let ratingsChangeDueToCompletionEfficiency;
   try {
-    const sqlUpdateNewRating = `UPDATE profiles SET rating = ? WHERE id = ?`
+    const sqlCompleteTask = `SELECT * FROM tasks WHERE taskId = ?`;
+    const queryResults = await database.query(sqlCompleteTask, [taskId]);
+    startTime = queryResults[0][0].taskStartTime;
+    endTime = queryResults[0][0].taskEndTime;
+    expectedTaskDurationInHours = queryResults[0][0].expectedTaskDurationInHours;
+    ratingsChangeDueToCompletionEfficiency =
+      (expectedTaskDurationInHours - (new Date(endTime) - new Date(startTime)) / (1000 * 60 * 60)) / 100;
+  } catch (err) {
+    console.log(err);
+    return next(err);
+  }
+
+  console.log('old rating: ' + oldRating);
+  // calculate rating based on feedback and completion efficiency while making sure it is between 0 and 5
+  calculatedRating = Math.min(
+    Math.max(oldRating * 0.8 + newRating * 0.2 + ratingsChangeDueToCompletionEfficiency, 0),
+    5
+  );
+  console.log('calculated rating: ' + calculatedRating);
+  try {
+    const sqlUpdateNewRating = `UPDATE profiles SET rating = ? WHERE id = ?`;
     const updateResults = await database.query(sqlUpdateNewRating, [calculatedRating, req.userId]);
     return res.json({ success: updateResults[0].affectedRows > 0 });
   } catch (err) {

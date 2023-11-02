9f82e75544895799ee9501ea07a7721342056627
alan <alanshen111@gmail.com>
2023-11-01 22:56:31 -0700

    arER

--- frontend/app/src/main/java/com/plotpals/client/ForumBoardFeedbackActivity.java
+++ frontend/app/src/main/java/com/plotpals/client/ForumBoardFeedbackActivity.java
@@ -3,10 +3,22 @@ package com.plotpals.client;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.plotpals.client.utils.GoogleProfileInformation;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ForumBoardFeedbackActivity extends NavBarActivity {
 
@@ -37,6 +49,40 @@ public class ForumBoardFeedbackActivity extends NavBarActivity {
         title.setText(taskTitle);
         TextView name = findViewById(R.id.forum_board_feedback_name);
         name.setText(taskAssignee);
+    }
+
+    private void sendFeedback() {
+
+        RequestQueue volleyQueue = Volley.newRequestQueue(this);
+        HashMap<String, String> params = new HashMap<>();
+
+        String url = "";
+
+        Request<?> jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.PUT,
+                url,
+                new JSONObject(params),
+                (JSONObject response) -> {
+                    try {
+                        Log.d(TAG, "Response for submitting form: \n"
+                                + response.getString("success"));
+                    } catch (JSONException e) {
+                        Log.d(TAG, e.toString());
+                    }
+                },
+                (VolleyError e) -> {
+                    Log.d(TAG, e.toString());
+                }
+        ) {
+            @Override
+            public Map<String, String> getHeaders() {
+                HashMap<String, String> headers = new HashMap<>();
+                headers.put("Authorization", "Bearer " + googleProfileInformation.getAccountIdToken());
+                return headers;
+            }
+        };
+
+        volleyQueue.add(jsonObjectRequest);
     }
 
     /**

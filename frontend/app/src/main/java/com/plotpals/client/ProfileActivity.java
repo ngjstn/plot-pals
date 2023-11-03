a011aee4a3818662de32f7692bd9bf9de6266a57
Justin Ng <ngjustin2002@gmail.com>
2023-11-02 23:52:51 -0700

    updated edit profile UI

--- frontend/app/src/main/java/com/plotpals/client/ProfileActivity.java
+++ frontend/app/src/main/java/com/plotpals/client/ProfileActivity.java
@@ -1,14 +1,111 @@
 package com.plotpals.client;
 
+import static com.plotpals.client.GardenInfoMemberActivity.googleProfileInformation;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.plotpals.client.data.Profile;
+import com.plotpals.client.utils.GoogleProfileInformation;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ProfileActivity extends AppCompatActivity {
+    final static String TAG = "ProfileActivity";
+    GoogleProfileInformation googleProfileInformation;
+    ImageView AccountProfilePictureImageView;
+    TextView AccountProfileNameTextView;
+    TextView GoogleNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        loadExtras();
+        requestProfileInformation();
+
+        AccountProfileNameTextView = findViewById(R.id.account_name_text_view);
+
+        GoogleNameTextView = findViewById(R.id.name);
+        GoogleNameTextView.setText(googleProfileInformation.getAccountGoogleName());
+
+        AccountProfilePictureImageView = findViewById(R.id.account_pic_image_view);
+        Picasso.get()
+                .load(googleProfileInformation.getAccountGoogleProfilePictureImageUrl())
+                .fit()
+                .centerCrop()
+                .placeholder(R.drawable.default_profile_picture)
+                .error(R.drawable.default_profile_picture)
+                .into(AccountProfilePictureImageView);
+
+        findViewById(R.id.close_icon).setOnClickListener(view -> finish());
+        findViewById(R.id.check_icon).setOnClickListener(view -> finish());
+    }
+
+    private void requestProfileInformation() {
+        RequestQueue volleyQueue = Volley.newRequestQueue(this);
+        String url = "http://10.0.2.2:8081/profiles/all?profileId=" + googleProfileInformation.getAccountUserId();
+
+        Request<?> jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null,
+
+                (JSONObject response) -> {
+                    try {
+                        Log.d(TAG, "Obtaining account profile information");
+                        JSONArray fetchedProfile = (JSONArray)response.get("data");
+
+                        Log.d(TAG, fetchedProfile.toString());
+
+                        if (fetchedProfile.length() > 0) {
+                            /* Use fetched profile information to populate page with relevant account information */
+                            Profile profile = new Profile(fetchedProfile.getJSONObject(0));
+                            AccountProfileNameTextView.setText(profile.getDisplayName());
+                        }
+
+                    } catch (JSONException e) {
+                        Log.d(TAG, e.toString());
+                    }
+                },
+                (VolleyError e) -> {
+                    Log.d(TAG, e.toString());
+                }
+        ) {
+            @Override
+            public Map<String, String> getHeaders() {
+                HashMap<String, String> headers = new HashMap<>();
+                headers.put("Authorization", "Bearer " + googleProfileInformation.getAccountIdToken());
+                return headers;
+            }
+        };
+
+        volleyQueue.add(jsonObjectRequest);
+    }
+
+    /**
+     * load extras forwarded from previous activity
+     */
+    private void loadExtras() {
+        Bundle extras = getIntent().getExtras();
+
+        if (extras != null) {
+            googleProfileInformation = new GoogleProfileInformation(extras);
+        }
     }
 }

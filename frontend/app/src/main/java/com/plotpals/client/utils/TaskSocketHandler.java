6d6fae3a791f996c83b58cdb830528d8db7d9ec7
alan <alanshen111@gmail.com>
2023-11-20 16:18:13 -0800

    using mock account

--- frontend/app/src/main/java/com/plotpals/client/utils/TaskSocketHandler.java
+++ frontend/app/src/main/java/com/plotpals/client/utils/TaskSocketHandler.java
@@ -13,7 +13,7 @@ public class TaskSocketHandler {
 
     public static synchronized void setSocket() {
         try {
-            taskSocket = IO.socket("https://xqx6apo57k.execute-api.us-west-2.amazonaws.com/");
+            taskSocket = IO.socket("http://10.0.2.2:8081/");
         } catch (URISyntaxException e) {
             Log.d(TAG, "Error setting socket");
         }
